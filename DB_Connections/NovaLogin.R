2de13f032dd98f29be3ef32578d995885a72b9ae
George Maynard <george.maynard@noaa.gov>
2021-04-16 14:59:01 -0400

    Connection scripts for all three servers as well as dialogue script

---
+++ DB_Connections/NovaLogin.R
@@ -0,0 +1,58 @@
+## ---------------------------
+## Script name: NovaLogin.R
+##
+## Purpose of script: creates a connection object ("con") that allows for data
+##    transfers to and from the NEFSC Oracle databases
+##
+## Author: George A. Maynard
+##
+## Date Created: 2021-03-19
+##
+## Email: george.maynard@noaa.gov
+##
+## ---------------------------
+## Notes: You must be on the VPN and have appropriate credentials for this to 
+##    work
+##
+## ---------------------------
+## Set Working Directory
+##
+## ---------------------------
+## Block scientific notation
+options(scipen = 6, digits = 4)
+## ---------------------------
+## Load necessary packages
+library(getPass)
+library(odbc)
+library(keyring)
+## ---------------------------
+## Load necessary functions
+source("varEntryDialog.r")
+## ---------------------------
+## Check the system information to determine login type
+if(Sys.info()[[4]]=="NECLWH04656117"&&Sys.info()[[7]]=="george.maynard"){
+  con=dbConnect(
+    odbc::odbc(),
+    .connection_string=paste0(
+      "DRIVER={Oracle in instantclient_12_2};DBQ=nova.nefsc.noaa.gov:1526/nova;UID=gmaynard;PWD=",
+      keyring::backend_file$new()$get(
+        service="SOLE",
+        user="gmaynard",
+        keyring="GMaynard_keyring"
+      )
+    ),
+    timeout=10
+  )
+} else {
+  username=varEntryDialog(vars="Username: ")
+  con=dbConnect(
+    odbc::odbc(),
+    .connection_string=paste0(
+      "DRIVER={Oracle in instantclient_12_2};DBQ=nova.nefsc.noaa.gov:1526/nova;UID=",
+      username,
+      ";PWD=",
+      getPass(msg='PASSWORD')
+    ),
+    timeout=10
+  )
+}
